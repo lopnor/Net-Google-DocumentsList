c8cd78d2ba6cae69e60f557b43a939b99c3c941c
Nobuo Danjou <danjou@luisa.local>
2009-09-22 20:08:31 +0900

    initial commit

---
+++ lib/Net/Google/DocumentsList.pm
@@ -0,0 +1,33 @@
+package Net::Google::DocumentsList;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Net::Google::DocumentsList -
+
+=head1 SYNOPSIS
+
+  use Net::Google::DocumentsList;
+
+=head1 DESCRIPTION
+
+Net::Google::DocumentsList is
+
+=head1 AUTHOR
+
+Noubo Danjou E<lt>nobuo.danjou@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
