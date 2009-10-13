9e2e6603e22a537326e63706dd48bbb66c80fb8b
Nobuo Danjou <danjou@luisa.local>
2009-10-13 22:38:57 +0900

    feedurl for documents

--- lib/Net/Google/DocumentsList.pm
+++ lib/Net/Google/DocumentsList.pm
@@ -1,8 +1,27 @@
 package Net::Google::DocumentsList;
+use Moose;
+use Net::Google::DataAPI;
 
-use strict;
-use warnings;
 our $VERSION = '0.01';
+
+with 'Net::Google::DataAPI::Role::Service' => {
+    service => 'writely',
+    source => __PACKAGE__.'-'.$VERSION,
+    gdata_version => '3.0',
+    ns => {
+        gAcl => 'http://schemas.google.com/acl/2007',
+        batch => 'http://schemas.google.com/gdata/batch',
+        docs => 'http://schemas.google.com/docs/2007',
+
+
+    }
+};
+
+feedurl document => (
+    entry_class => 'Net::Google::DocumentsList::Document',
+    default => 'http://docs.google.com/feeds/documents/private/full',
+    is => 'ro',
+);
 
 1;
 __END__
---
+++ lib/Net/Google/DocumentsList/Document.pm
@@ -0,0 +1,6 @@
+package Net::Google::DocumentsList::Document;
+use Moose;
+use Net::Google::DataAPI;
+with 'Net::Google::DataAPI::Role::Entry';
+
+1;
