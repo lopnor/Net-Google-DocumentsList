b8943c9bbb4355846c2ad96d99522781f8ff48a9
Nobuo Danjou <danjou@soffritto.org>
2012-08-02 18:05:26 +0900

    metadata pods

--- lib/Net/Google/DocumentsList/Metadata.pm
+++ lib/Net/Google/DocumentsList/Metadata.pm
@@ -49,3 +49,115 @@ __PACKAGE__->meta->make_immutable;
 no Any::Moose;
 
 1;
+__END__
+
+=head1 NAME
+
+Net::Google::DocumentsList::Metadata - metadata object for Google Documents List Data API
+
+=head1 SYNOPSIS
+
+  use Net::Google::DocumentsList;
+  
+  my $client = Net::Google::DocumentsList->new(
+      username => 'myname@gmail.com',
+      password => 'p4$$w0rd'
+  );
+
+  my $metadata = $client->metadata;
+
+
+=head1 DESCRIPTION
+
+This module represents metadata object for Google Documents List Data API
+
+=head1 ATTRIBUTES
+
+=head2 updated
+
+returns updated time
+
+=head2 largest_changestamp
+
+returns largest changestamp: you can use this attribute to get latest changes from change feeds
+
+  my $client = Net::Google::DocumentsList->new(...);
+  my $largest_changestamp = $client->metadata->largest_changestamp;
+  my @changes = $client->changes({'start-index' => $largest_changestamp - 10, 'max-results' => 10});
+
+see also: L<Net::Google::DocumentsList::Change>
+
+=head2 quota_bytes_total
+
+returns total quota bytes you can use
+
+=head2 quota_bytes_used
+
+returns total quota bytes you've already used
+
+=head2 quota_bytes_used_in_trash
+
+returns total quota bytes you've already used with items in trash
+
+=head2 max_upload_size
+
+returns max upload size for each services in hashref format:
+
+  my $metadata = $client->metadata;
+  my $max = $metadata->max_upload_size;
+  # returns (for example):
+  # {
+  #   'document' => '2097152',
+  #   'drawing' => '2097152',
+  #   'file' => '10737418240',
+  #   'pdf' => '10737418240',
+  #   'presentation' => '52428800',
+  #   'spreadsheet' => '20971520'
+  # }
+
+  my $file = '/path/to/your.pdf';
+  if (-s $file > $max->{pdf}) {
+    die 'you can not upload this pdf because it is too large';
+  }
+  # now you can upload the file safely
+
+  $client->add_item({file => $file});
+
+=head2 import_format
+
+returns map of file formats and types of Google Docs items to be converted:
+
+  my $map = $client->metadata->import_format;
+  # returns (for example):
+  # {
+  #   '' => [''],
+  # }
+
+=head2 export_format
+
+returns map of types of items and exportable formats:
+
+  my $map = $client->metadata->import_format;
+  # returns (for example):
+  # {
+  #   '' => [''],
+  # }
+
+=head1 AUTHOR
+
+Noubo Danjou E<lt>nobuo.danjou@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+L<XML::Atom>
+
+L<Net::Google::DataAPI>
+
+L<https://developers.google.com/google-apps/documents-list/>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
